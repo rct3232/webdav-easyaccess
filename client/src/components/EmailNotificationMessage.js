bbff967642b46679736237a0557d8726c8235fbd
rct3232 <rct3232@gmail.com>
2025-12-04 08:31:29 +0900

    fix: remove Typography nesting in EmailNotificationMessage
    - Return plain text instead of Typography component to avoid nesting
    
    - EmailNotificationMessage now renders as text within parent Typography

--- client/src/components/EmailNotificationMessage.js
+++ client/src/components/EmailNotificationMessage.js
@@ -1,12 +1,7 @@
 import React from 'react';
-import { Typography } from '@mui/material';
 
 const EmailNotificationMessage = () => {
-  return (
-    <Typography variant="body2">
-      승인 결과는 입력하신 이메일로 안내드립니다.
-    </Typography>
-  );
+  return '승인 결과는 입력하신 이메일로 안내드립니다.';
 };
 
 export default EmailNotificationMessage;
